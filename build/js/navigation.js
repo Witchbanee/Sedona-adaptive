7fc0b6598f4ef2ac741bd1a1f14df27e27f9d33c
Pavel <danshin-pavel@mail.ru>
2023-03-04 14:38:28 +0300

    more fixes

--- build/js/navigation.js
+++ build/js/navigation.js
@@ -1,8 +1,10 @@
 let nav = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let mainNav = document.querySelector(".main-nav__list");
 
+mainNav.classList.remove("main-nav__list--no-js")
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", function () {
   if (nav.classList.contains("main-nav--close")) {
     nav.classList.remove("main-nav--close");
     nav.classList.add("main-nav--open");
